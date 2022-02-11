a15b4c09d5e1fbc3af11836b3935a90c849ac9e0
Kartavya101 <81504383+Kartavya101@users.noreply.github.com>
2022-02-11 20:16:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7UEx8CpAYRtb1erVDnIWaiUrckA7eSXI",
+  authDomain: "storytelling-1d4f3.firebaseapp.com",
+  projectId: "storytelling-1d4f3",
+  storageBucket: "storytelling-1d4f3.appspot.com",
+  messagingSenderId: "607206224176",
+  appId: "1:607206224176:web:78d3a5e14396117de158f5"
+};
